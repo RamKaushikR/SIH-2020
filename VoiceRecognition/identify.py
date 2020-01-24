778c75bba37ef7cee3db9b93ad02b9aa9098401d
pbcpraveen <pbcpraveen@gmail.com>
2020-01-25 00:01:38 +0530

    intergated denoising and added a bash script to execute flask

--- VoiceRecognition/identify.py
+++ VoiceRecognition/identify.py
@@ -10,10 +10,31 @@ from sklearn.utils.extmath import softmax
 #from mfcc import MFCC
 import warnings
 import time
-from Denoiser import Denioser as denoise
+
 
 warnings.filterwarnings('ignore')
-
+class Denoiser:
+    """
+    denoises the input voice signal using frequency component anlysis
+    """
+    def __init__(self, inData = None,keep_fraction = 0.3):
+        self.data = inData
+        self.keep_fraction = keep_fraction
+    def fftdenoise(self):
+        """
+        The function transfroms the time domain signal to a frequency domain and retains a fraction of  frequency                           specified.Returns the denoised time domain signal.
+        Parameters:
+        :exclude_fraction the fraction of frequency to be excluded 
+        Return Value: return np.unit16 value of the time domain signal
+        """
+        s = sp.fft(self.data)
+        m = np.mean(s)
+        exclude = 0.01
+        im_fft2 = s.copy()
+        r = len(im_fft2)
+        im_fft2[int(r*self.keep_fraction):int(r*(1-self.keep_fraction))] = 0
+        im_new = sp.ifft(im_fft2).real
+        return np.int16(im_new)
 
 class MFCC:
     """
