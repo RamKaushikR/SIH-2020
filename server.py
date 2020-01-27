202f9c4d831b4ae964972679b6ea53027c6765f4
ramkaushikr <ramkaushik17125@cse.ssn.edu.in>
2020-01-27 21:52:22 +0530

    Real Fake

--- server.py
+++ server.py
@@ -18,8 +18,6 @@ for fakeness detection
 protoPath = os.path.sep.join(["face_detector", "deploy.prototxt"])
 modelPath = os.path.sep.join(["face_detector", "res10_300x300_ssd_iter_140000.caffemodel"])
 net = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
-
-print("[INFO] loading liveness detector...")
 model = load_model("liveness.model")
 le = pickle.loads(open("le.pickle", "rb").read())
 
@@ -39,58 +37,6 @@ if 'faces' not in directories:
     os.mkdir('faces')
 
 app = Flask(__name__)
-
-
-@app.route('/isFake', methods = ['GET', 'POST'])
-def isFake():
-    img = request.files['face_image']
-    target = request.form['target']
-    
-    img.save('predict/img.jpg')
-    print('Received File for fake detection')
-    frame = cv2.imread('predict/img.jpg')
-    frame = imutils.resize(frame, width=600)
-    (h, w) = frame.shape[:2]
-    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
-    net.setInput(blob)
-    detections = net.forward()
-    for i in range(0, detections.shape[2]):
-        # extract the confidence (i.e., probability) associated with the
-        # prediction
-        confidence = detections[0, 0, i, 2]
-
-        # filter out weak detections
-        if confidence > args["confidence"]:
-            # compute the (x, y)-coordinates of the bounding box for
-            # the face and extract the face ROI
-            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-            (startX, startY, endX, endY) = box.astype("int")
-            # ensure the detected bounding box does fall outside the
-            # dimensions of the frame
-            startX = max(0, startX)
-            startY = max(0, startY)
-            endX = min(w, endX)
-            endY = min(h, endY)
-            # extract the face ROI and then preproces it in the exac
-            # same manner as our training data
-            face = frame[startY:endY, startX:endX]
-            face = cv2.resize(face, (32, 32))
-            face = face.astype("float") / 255.0
-            face = img_to_array(face)
-            face = np.expand_dims(face, axis=0)
-
-            # pass the face ROI through the trained liveness detector
-            # model to determine if the face is "real" or "fake"
-            preds = model.predict(face)[0]
-            j = np.argmin(preds)
-            label = le.classes_[j]
-
-            # draw the label and bounding box on the frame
-            verdict = label.decode("utf-8")
-            if(verdict=='real'):
-                return 1
-            else:
-                return 0
 
 
 @app.route('/findSpeaker', methods = ['GET', 'POST'])
@@ -185,6 +131,63 @@ def removeSpeaker():
     r = {'result': result}
     return jsonify(r)
 
+def isFake(path):
+    """
+    img = request.files['face_image']
+    target = request.form['target']
+    
+    img.save('predict/img.jpg')
+    print('Received File for fake detection')
+    """
+    global net, model, le
+
+    frame = cv2.imread(path)
+    frame = imutils.resize(frame, width = 600)
+
+    (h, w) = frame.shape[: 2]
+
+    blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
+    net.setInput(blob)
+    detections = net.forward()
+
+    for i in range(0, detections.shape[2]):
+        # extract the confidence (i.e., probability) associated with the
+        # prediction
+        confidence = detections[0, 0, i, 2]
+
+        # filter out weak detections
+        if confidence > 0.5:
+            # compute the (x, y)-coordinates of the bounding box for
+            # the face and extract the face ROI
+            box = detections[0, 0, i, 3: 7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+            # ensure the detected bounding box does fall outside the
+            # dimensions of the frame
+            startX = max(0, startX)
+            startY = max(0, startY)
+            endX = min(w, endX)
+            endY = min(h, endY)
+            # extract the face ROI and then preproces it in the exac
+            # same manner as our training data
+            face = frame[startY: endY, startX: endX]
+            face = cv2.resize(face, (32, 32))
+            face = face.astype("float") / 255.0
+            face = img_to_array(face)
+            face = np.expand_dims(face, axis = 0)
+
+            # pass the face ROI through the trained liveness detector
+            # model to determine if the face is "real" or "fake"
+            preds = model.predict(face)[0]
+            j = np.argmin(preds)
+            label = le.classes_[j]
+
+            # draw the label and bounding box on the frame
+            verdict = label.decode("utf-8")
+            if(verdict == 'real'):
+                return 1
+            return -1
+    return -1
+
 @app.route('/findFace', methods = ['GET','POST'])
 def findFace():
     #process the image here.. Need to get specification from KabishQ
@@ -194,8 +197,13 @@ def findFace():
     img.save('predict/img.jpg')
     print('Received File')
 
-    fr = Face_Identify(source = 'predict/')
-    result = fr.identify_face(target)
+    real = isFake('predict/img.jpg')
+
+    if real == -1:
+        result = -1
+    else:
+        fr = Face_Identify(source = 'predict/')
+        result = fr.identify_face(target)
         
     r = {'result': result}
     return jsonify(r)
